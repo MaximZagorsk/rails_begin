e3efd6ebb35ef2cbeb9dd66a8e2d59983a3eb42c
MaksimZV <zagorskiymv@yandex.ru>
2021-05-18 14:40:17 +0300

    Unused files removed and static html files moved

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,2 @@
 Rails.application.routes.draw do
-  get "/about/author", to: "author#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  get "/about", to: "about#index"
-
 end
