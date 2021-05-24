15cd3136c65aa53f48cb2fc4b93ae2d6eddf3ffc
MaksimZV <zagorskiymv@yandex.ru>
2021-05-24 20:27:26 +0300

    Create Test query class method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.tests_by_category(category)
+    category_id = Category.where(title: category)
+    test = Test.where(category_id: category_id[0].id).order("id DESC")
+  end
 end
