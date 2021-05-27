3a04f45604cc9b93a73cfb7a647e532296243f59
MaksimZV <zagorskiymv@yandex.ru>
2021-05-27 15:37:14 +0300

    Changing with use rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def ending_test_by_level(level)
-    Test.joins("INNER JOIN results ON results.test_id = tests.id").
-where("results.user_id = ?", self.id).where("tests.level = ?",level)
+    Test.joins('INNER JOIN results ON results.test_id = tests.id')
+        .where('results.user_id = ?', id).where('tests.level = ?', level)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,33 +6,36 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+category_rails = Category.create!(title: 'Rails')
+category_django = Category.create!(title: 'Django')
+category_javascript = Category.create!(title: 'JavaScript')
 
-category_rails = Category.create!( title: "Rails")
-category_django = Category.create!(title: "Django")
-category_javascript = Category.create!( title: "JavaScript")
+user_Ivan = User.create!(username: 'Ivan', password: 453_213)
+user_Vano = User.create!(username: 'Vano', password: 983_532)
+user_Jack = User.create!(username: 'Jack', password: 214_567)
 
-user_Ivan = User.create!( username: "Ivan", password: 453213 )
-user_Vano = User.create!( username: "Vano", password: 983532 )
-user_Jack = User.create!( username: "Jack", password: 214567 )
+test_rails = Test.create!(title: 'Rails begin', level: 0, category_id: category_rails.id, author_id: user_Ivan.id)
+test_django = Test.create!(title: 'Django begin', level: 0, category_id: category_django.id, author_id: user_Ivan.id)
+test_javascript = Test.create!(title: 'JavaScript begin', level: 0, category_id: category_javascript.id,
+                               author_id: user_Ivan.id)
 
-test_rails = Test.create!(title: "Rails begin", level: 0, category_id: category_rails.id, author_id: user_Ivan.id )
-test_django = Test.create!(title: "Django begin", level: 0, category_id: category_django.id, author_id: user_Ivan.id )
-test_javascript = Test.create!(title: "JavaScript begin", level: 0, category_id: category_javascript.id, author_id: user_Ivan.id )
-
-question_djangobegin = Question.create!(title: "Django is backend framework?", test_id: test_django.id)
-question_railsbegin = Question.create!(title: "Rails is backend framework?", test_id: test_django.id)
-question_javascriptbegin = Question.create!(title: "JavaScript is backend framework?", test_id: test_django.id)
+question_djangobegin = Question.create!(title: 'Django is backend framework?', test_id: test_django.id)
+question_railsbegin = Question.create!(title: 'Rails is backend framework?', test_id: test_django.id)
+question_javascriptbegin = Question.create!(title: 'JavaScript is backend framework?', test_id: test_django.id)
 
 answer_qdb = Answer.create!([
-  {title: "Yes", correct: true, question_id: question_djangobegin.id},
-  {title: "No", correct: false, question_id: question_djangobegin.id}])
+                              { title: 'Yes', correct: true, question_id: question_djangobegin.id },
+                              { title: 'No', correct: false, question_id: question_djangobegin.id }
+                            ])
 
 answer_qrb = Answer.create!([
-  {title: "Yes", correct: true, question_id: question_railsbegin.id},
-  {title: "No", correct: false, question_id: question_railsbegin.id}])
-  
+                              { title: 'Yes', correct: true, question_id: question_railsbegin.id },
+                              { title: 'No', correct: false, question_id: question_railsbegin.id }
+                            ])
+
 answer_qjsb = Answer.create!([
-  {title: "Yes", correct: true, question_id: question_javascriptbegin.id},
-  {title: "No", correct: false, question_id: question_javascriptbegin.id}])
+                               { title: 'Yes', correct: true, question_id: question_javascriptbegin.id },
+                               { title: 'No', correct: false, question_id: question_javascriptbegin.id }
+                             ])
 
 result_ivan = Result.create!(test_id: test_rails.id, user_id: user_Ivan.id, ending: true)
