420b1f18ed1072de60aa94a2ee30b132c3f9d14d
MaksimZV <zagorskiymv@yandex.ru>
2021-05-28 19:47:00 +0300

    INNER removed from models files

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   def self.tests_by_category(category)
-    Test.joins('INNER JOIN categories ON categories.id = tests.category_id')
+    Test.joins('JOIN categories ON categories.id = tests.category_id')
         .where('categories.title = ?', category)
         .order(title: :desc)
   end
